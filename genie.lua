05a69d1ba3e3c340662df5d67745ceac5cdf85ad
Tommy Carlsson <topcatse@yahoo.se>
2017-03-20 01:10:15 +0100

    Hello World

---
+++ genie.lua
@@ -0,0 +1,32 @@
+BASE_DIR   = path.getabsolute(".")
+BUILD_PATH = path.join(BASE_DIR, "build")
+
+solution "TickCounterSolution"
+	location(path.join(BUILD_PATH, _ACTION))
+	targetdir(path.join(BUILD_PATH, "bin"))
+
+	configurations {
+		"Release",
+		"Debug"
+	}
+	
+	platforms {
+		"Native", -- for targets where bitness is not specified
+	}
+
+configuration "Debug"
+	flags       { "Symbols" }
+	defines     { "DEBUG=1" }
+	
+configuration "Release"
+	flags       { "OptimizeSize" }
+
+project "TickCounter"
+	language "C++"
+	kind     "ConsoleApp"
+	
+	files {
+		"TickCounter/*.cpp", "TickCounter/*.h"
+
+	}
+
